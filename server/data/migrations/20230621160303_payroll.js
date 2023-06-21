913257b8845d74ceb74764a3ff520327920aba42
Joshua Hammond <j.hammond@erasolutions.us>
2023-06-21 17:28:42 -0500

    create job training and payroll table schema

---
+++ server/data/migrations/20230621160303_payroll.js
@@ -0,0 +1,36 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('payroll', table => {
+        table.increments('pr_id').primary()
+    
+        table.integer('pr_user_id').unsigned
+        
+        table.decimal('pr_work_hours',4).notNullable()
+    
+        table.decimal('pr_hourly_rate', 4).notNullable()
+    
+        table.decimal('pr_contrat_pay', 6).notNullable()
+
+        table.string('pr_banking_name', 30).notNullable()
+
+        table.string('pr_banking_routing', 9).notNullable()
+
+        table.string('pr_banking_account', 12).notNullable()
+
+
+      })
+};
+    
+
+
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+};
