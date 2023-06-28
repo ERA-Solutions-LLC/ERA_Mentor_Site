58d0a1d2b22cd4f1124b5963d00d940fef67a93a
AJWD92 <wesley.gossett@gmail.com>
2023-06-28 13:00:13 -0400

    Created a login form to be used for the hr and employees will not merge this to main yet till appretince has a chance to try and add logic plus styling to the template.

--- src/Components/Form/FormSchema.jsx
+++ src/Components/Form/FormSchema.jsx
@@ -2,7 +2,7 @@ import * as Yup from 'yup';
 
 const phoneRegex = RegExp(/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/);
 
-// const passwordRegex = RegExp(/^\(?(=.*\d)(?=.*[!@#$%^&*]))
+const passwordRegex = RegExp(/^\(?(=.*\d)(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/);
 
 export const ContactFormStyle01Schema = Yup.object().shape({
     name: Yup.string().required("Field is required."),
@@ -32,4 +32,9 @@ export const ContactFormStyle05Schema = Yup.object().shape({
     email: Yup.string().email("Invalid email.").required("Field is required."),
     phone: Yup.string().required().matches(phoneRegex, "Invalid phone"),
     terms_condition: Yup.boolean().oneOf([true],'Message').required()
+});
+
+export const ContactFormStyle06Schema = Yup.object().shape({
+    email: Yup.string().email("Invalid email.").required("Field is required."),
+    password: Yup.string().required().matches(passwordRegex, "Invalid password please make sure to make a password that is 6-16 characters long and contains as least 1 uppercase letter, 1 lowercase letter, 1 special character, and 1 number."),
 });
---
+++ src/Pages/Home/Startup/EraLoginForm.jsx
@@ -0,0 +1,98 @@
+import React, { useState } from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+const LoginForm = () => {
+  const history = useNavigate();
+  const [error, setError] = useState('');
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: ''
+    },
+    validationSchema: Yup.object().shape({
+      email: Yup.string().email('Invalid email format.').required('Email is required.'),
+      password: Yup.string()
+        .matches(
+          /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/,
+          'Invalid password. Please make sure it is 6-16 characters long and contains at least 1 uppercase letter, 1 lowercase letter, 1 special character, and 1 number.'
+        )
+        .required('Password is required.')
+    }),
+    onSubmit: async (values) => {
+      try {
+        const response = await axios.post('/api/login', values);
+        const { is_hr } = response.data.is_hr;
+
+        if (is_hr) {
+            history.push('/hr-dashboard'); // Redirect to HrDashboard component
+        } else {
+            history.push('/employee-dashboard'); // Redirect to EmployeeDashboard component
+        }
+      } catch (error) {
+        setError('Invalid email or password.'); // Set error message based on server response
+      }
+    }
+  });
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <form className="w-2/5 bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={formik.handleSubmit}>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+            Email
+          </label>
+          <input
+            className={`appearance-none border ${
+              formik.touched.email && formik.errors.email ? 'border-red-500' : 'border-gray-300'
+            } rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`}
+            id="email"
+            name="email"
+            type="email"
+            placeholder="Enter your email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          {formik.touched.email && formik.errors.email && (
+            <p className="text-red-500 text-xs italic">{formik.errors.email}</p>
+          )}
+        </div>
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+            Password
+          </label>
+          <input
+            className={`appearance-none border ${
+              formik.touched.password && formik.errors.password ? 'border-red-500' : 'border-gray-300'
+            } rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`}
+            id="password"
+            name="password"
+            type="password"
+            placeholder="Enter your password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          {formik.touched.password && formik.errors.password && (
+            <p className="text-red-500 text-xs italic">{formik.errors.password}</p>
+          )}
+        </div>
+        {error && <p className="text-red-500 text-xs italic">{error}</p>}
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Sign In
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
