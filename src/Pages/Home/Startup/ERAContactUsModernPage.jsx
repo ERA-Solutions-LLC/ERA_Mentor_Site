f217b2138b1ff4dca111d8c594b1c9fd43e34151
AJWD92 <wesley.gossett@gmail.com>
2023-07-07 07:44:07 -0400

    re ran build folder.

--- src/Pages/Home/Startup/ERAContactUsModernPage.jsx
+++ src/Pages/Home/Startup/ERAContactUsModernPage.jsx
@@ -78,19 +78,13 @@ const ContactUsModernPage = (props) => {
       <SideButtons />
       {/* Section Start */}
       <section className="bg-gradient-to-tr from-[#3975f6] via-[#FFF] to-[#f3aaca] py-[130px] lg:py-[90px] md:py-[75px] sm:py-[50px]">
-        <Container className="h-[300px] sm:h-[250px] flex justify-center sm:justify-end">
-          <Row xs={1} md={2} className="items-end justify-end sm:flex-col">
-            <Col className="p-0 sm:!px-[15px] sm:mb-[20px]">
-              <h1 className="text-xmd !leading-[20px] pl-[90px] pr-[15px] text-darkgray font-serif mb-0 relative sm:pl-[55px] md:mb-0 sm:mb-[20px]">
-                {/*<span className="h-[1px] absolute top-[12px] left-0 w-[70px] bg-black sm:w-[40px] sm:top-1/2"></span>*/}
-                
-              </h1>
-            </Col>
-            <Col className="p-0 sm:!px-[15px]">
-              <h4 className="font-medium text-slate-50 bg-black opacity-60  font-serif mb-0">Sounds great, but how do I sign up?</h4>
-            </Col>
-          </Row>
-        </Container>
+      <Container className="h-[300px] sm:h-[250px] flex justify-center items-center">
+        <Row xs={1} md={2} className="items-end justify-center sm:flex-col">
+          <Col className="p-0 sm:!px-[15px]">
+            <h4 className="font-medium text-black font-serif mb-0">Sounds great, but how do I sign up?</h4>
+          </Col>
+        </Row>
+      </Container>
       </section>
       {/* Section End */}
 
