67e9de343cb7ba391af1f045b84ba46425bfa627
a-rosado <s.rosado-coll@erasolutions.us>
2023-07-07 17:01:22 -0500

    updated home page ERA tittle

--- src/Pages/Home/Startup/ERABackgroundVid.jsx
+++ src/Pages/Home/Startup/ERABackgroundVid.jsx
@@ -12,7 +12,7 @@ const ERABackgroundVid = () => {
     
     return (
     <Videos
-      className=" w-full h-full py-[550px] pb-0 xl:py-[120px] md:py-[75px] sm:py-[80px] xs:py-0 h-[700px] xl:h-[700px] md:h-[556px] sm:h-auto xs:h-[26vh]"
+      className=" w-full h-full py-[550px] pb-0 xl:py-[120px] md:py-[75px] sm:py-[11px] xs:py-0 h-[700px] xl:h-[700px] md:h-[556px] sm:h-auto xs:h-[26vh]"
       src={bgVideo}
       loop={true}
       poster="/assets/img/mfg-close.png">
@@ -21,7 +21,7 @@ const ERABackgroundVid = () => {
           <Col xl={6} lg={7} md={10} className="relative z-[2] text-center">
             {/* <span className="mb-[5px] font-serif text-white  block">Talent Solutions and Training</span> */}
             {/* <h1 className="text-white font-Poppins font-black font-medium tracking-widest text-[70px] leading-[72px] mb-28 sm:mb-12 sm:text-[30px] sm:leading-[40px] xs:text-[20px]">Era Solutions</h1> */}
-            <img className='sm:py-[100px] xs:py-[50px] xl:my-[200px] flex justify-center' alt='era svg text' src={Era3} />
+            <img className='sm:py-[150px] xs:hidden flex justify-center' alt='era svg text' src={Era3} />
             <span className="h-[80px] inline-block bg-white mb-20 sm:mb-12 sm:h-[50px]"></span>
           </Col>
         </Row>
